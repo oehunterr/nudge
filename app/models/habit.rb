df92a60d9fc80ab37fb80bb28ba1dc658c357424
joneswong <jones_wong@icloud.com>
2022-12-07 12:00:55 +0000

    search bar done

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -2,4 +2,21 @@ class Habit < ApplicationRecord
   belongs_to :user
   has_many :milestones, dependent: :destroy
   validates :title, :start_date, :end_date, presence: true
+
+  include PgSearch::Model
+  multisearchable against: [:title, :description]
+
+  pg_search_scope :search_by_title_and_description,
+    against: [ :title, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
+  pg_search_scope :global_search,
+    against: [ :title, :description ],
+    associated_against: {
+      user: [ :first_name, :last_name ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,4 +1,7 @@
 class Milestone < ApplicationRecord
   belongs_to :habit
   validates :title, :start_time, :end_time, presence: true
+
+  include PgSearch::Model
+  multisearchable against: [:title, :description]
 end
