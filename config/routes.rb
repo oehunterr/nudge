25af35f8e6647e90d8d9dc1b74f69949168f0e65
saleh youssef <saleh.youssef96@gmail.com>
2022-12-08 17:01:58 +0100

    link-to buttons

--- config/routes.rb
+++ config/routes.rb
@@ -20,3 +20,45 @@ end
 # ~
 # patch "habits/:id/active", to: "habits#active_toggle", as: :active_toggle
 # index, new, create, show, edit, update, destroy
+
+# <div class="grid first-line">
+#     <%= link_to habits_path do %>
+#       <div class="cards card-habit">
+#         <div class="habcolor">
+#           <div class="icons">
+#             <i class="fa-solid fa-business-time"></i>
+#           </div>
+#         </div>
+#         <h6>Habits</h6>
+#         <p><%= @milestone.size %> habits</p>
+#       </div>
+#     <%end%>
+#     <%= link_to do %>
+#       <div class="cards card-milestones">
+#         <div class="icons milcolour ">
+#           <i class="fa-solid fa-bullseye "></i>
+#         </div>
+#         <h6>Milestones</h6>
+#         <p><%= @milestone.size %> milestones</p>
+#       </div>
+#     <%end%>
+#   </div>
+#   <div class="grid second-line">
+#     <%= link_to do %>
+#       <div class="cards card-perfomance">
+#         <div class="icons perfomancecolor">
+#           <i class="fa-solid fa-chart-simple"></i>
+#         </div>
+#         <h6>Perfomance</h6>
+#       </div>
+#     <%end%>
+
+#     <%= link_to habits_path do %>
+#       <div class="cards card-groups">
+#             <div class="icons groupcolor">
+#               <i class="fa-solid fa-users-viewfinder"></i>
+#             </div>
+#         <h6>Groups</h6>
+#       </div>
+#     <%end%>
+#   </div>
