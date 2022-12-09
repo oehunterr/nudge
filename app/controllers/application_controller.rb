273b5281dc9c406026efd727e9e32b417441d98f
joneswong <jones_wong@icloud.com>
2022-12-09 07:28:11 +0000

    user stories, search habit and linked to habit show then create milestone within habit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
     if params[:query].present?
       sql_query = <<~SQL
         habits.title @@ :query
+        OR habits.id @@ :query
         OR habits.description @@ :query
         OR users.first_name @@ :query
         OR users.last_name @@ :query
